b114f697bbe50a2fa7f4ad396d0aab9bf44ea120
Giovanni Del Gaizo <delgagi@libero.it>
2025-04-22 15:31:40 +0200

    Corretto wsgi.py

--- playon/wsgi.py
+++ playon/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fubles_clone.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'palyon.settings')
 
 application = get_wsgi_application()
