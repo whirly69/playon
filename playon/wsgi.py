d098964197fa719916865d82c7b2f510ccff472e
Giovanni Del Gaizo <delgagi@libero.it>
2025-04-22 15:32:53 +0200

    Seconda correzione wsgi.py

--- playon/wsgi.py
+++ playon/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'palyon.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'playon.settings')
 
 application = get_wsgi_application()
